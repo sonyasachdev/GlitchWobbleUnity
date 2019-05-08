c0c3664cf08570b27eaa5163bc42fa3432d38097
sonyasachdev <sonyansachdev@gmail.com>
2019-05-08 04:30:15 -0400

    Disappearing Platforms and Player Stickiness
    Added the Active/Inactive states to platforms.
    Fixed the Stickiness on Player so that she doesn't stay stuck to walls.

--- Glitch Wobble/Assets/Scripts/Platforms/HorizontalPlatformMove.cs
+++ Glitch Wobble/Assets/Scripts/Platforms/HorizontalPlatformMove.cs
@@ -15,9 +15,18 @@ public class HorizontalPlatformMove : MonoBehaviour {
     public float rightBound;
     public float leftBound;
 
+    //Active State
+    private bool isActive;
+    public float activeTimer;
+    private float timeReset;
+    public SpriteRenderer sprender;
+
     // Use this for initialization
     void Start()
     {
+        isActive = true;
+        timeReset = activeTimer;
+        sprender = gameObject.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -41,6 +50,50 @@ public class HorizontalPlatformMove : MonoBehaviour {
         else
         {
             transform.position = new Vector2(transform.position.x - moveSpeed * Time.deltaTime, transform.position.y);
+        }
+
+        //Active Timer
+        //Platform On
+        if(isActive)
+        {
+            if (activeTimer > 0)
+            {
+                activeTimer -= Time.deltaTime;
+            }
+            else
+            {
+                //Turns off Platform
+                isActive = false;
+                gameObject.GetComponent<Collider2D>().enabled = false;
+                activeTimer = timeReset;
+
+
+                //Turns off Sprite
+                sprender.enabled = false;
+                Debug.Log("active");
+
+
+            }
+        }
+        //Platform Off
+        else
+        {
+            if (activeTimer > 0)
+            {
+                
+                activeTimer -= Time.deltaTime;
+            }
+            else
+            {
+                //Turns on Platform
+                isActive = true;
+                gameObject.GetComponent<Collider2D>().enabled = true;
+                activeTimer = timeReset;
+
+                //Turns on Sprite
+                sprender.enabled = true;
+                Debug.Log("inactive");
+            }
         }
 
     }
--- Glitch Wobble/Assets/Scripts/Platforms/VerticalPlatformMove.cs
+++ Glitch Wobble/Assets/Scripts/Platforms/VerticalPlatformMove.cs
@@ -16,10 +16,18 @@ public class VerticalPlatformMove : MonoBehaviour {
     public float upBound;
     public float downBound;
 
+    //Active State
+    private bool isActive;
+    public float activeTimer;
+    private float timeReset;
+    public SpriteRenderer sprender;
+
     // Use this for initialization
     void Start()
     {
-        //Makes platform start moving up
+        isActive = true;
+        timeReset = activeTimer;
+        sprender = gameObject.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -45,5 +53,46 @@ public class VerticalPlatformMove : MonoBehaviour {
             transform.position = new Vector2(transform.position.x, transform.position.y - moveSpeed * Time.deltaTime);
         }
 
+        //Active Timer
+        //Platform On
+        if (isActive)
+        {
+            if (activeTimer > 0)
+            {
+                activeTimer -= Time.deltaTime;
+            }
+            else
+            {
+                //Turns off Platform
+                isActive = false;
+                gameObject.GetComponent<Collider2D>().enabled = false;
+                activeTimer = timeReset;
+
+
+                //Turns off Sprite
+                sprender.enabled = false;
+                Debug.Log("active");
+            }
+        }
+        //Platform Off
+        else
+        {
+            if (activeTimer > 0)
+            {
+
+                activeTimer -= Time.deltaTime;
+            }
+            else
+            {
+                //Turns on Platform
+                isActive = true;
+                gameObject.GetComponent<Collider2D>().enabled = true;
+                activeTimer = timeReset;
+
+                //Turns on Sprite
+                sprender.enabled = true;
+                Debug.Log("inactive");
+            }
+        }
     }
 }
